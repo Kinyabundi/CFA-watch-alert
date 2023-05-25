2ac3e03e5447108308065a0950eccece3b2d10af
Christine Kinya Bundi <kinyachristine14@gmail.com>
2023-05-25 07:57:44 +0000

    fixed deployment error

--- backend/utils.mjs
+++ backend/utils.mjs
@@ -1,4 +1,4 @@
-import counties_data from "./data/counties.json" assert { type: "json" };
+import counties_data from "./data/counties.json";
 import * as turf from "@turf/turf";
 import _ from "lodash";
 
